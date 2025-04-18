8f14960bc6183ca3b51c17e753138dbf1f42aeff
Guillaume Bouly <gbouly1@gmail.com>
2025-04-18 14:53:01 +0200

    update shootingStar.css : delete star before and after to keep a star as line

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="container hero-wrapper">
       <h1>
         Guillaume B<span className="o-title">o</span>uly <br />
-        Fr<span className="o-title">o</span>ntend <br />
+        {/* Fr<span className="o-title">o</span>ntend <br /> */}
         Web devel<span className="o-title">o</span>per <br />
       </h1>
       <p className="title-location">Based in Paris</p>
